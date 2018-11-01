ab6ea113f04376a1499648d89eaede7e4062b2a6
Vaenkai <44669132+Vaenkai@users.noreply.github.com>
2018-11-01 12:43:02 +0200

    Add files via upload

---
+++ LabNM_1/LabNM_1/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace LabNM_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.StartButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            this.chart1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(2, 2);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series1.Color = System.Drawing.Color.Blue;
+            series1.Legend = "Legend1";
+            series1.Name = "Полином";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series2.Color = System.Drawing.Color.Red;
+            series2.Legend = "Legend1";
+            series2.MarkerSize = 10;
+            series2.Name = "Точки";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(721, 449);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // StartButton
+            // 
+            this.StartButton.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.StartButton.Location = new System.Drawing.Point(0, 457);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(721, 39);
+            this.StartButton.TabIndex = 1;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(721, 496);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button StartButton;
+        public System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ LabNM_1/LabNM_1/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LabNM_1
+{
+    public partial class Form1 : Form
+    {     
+        const int n = 10;
+        double[] x = new double[n];
+        double[] y = new double[n];
+        public Form1()
+        {
+            InitializeComponent();
+            int i;           
+            for (i = 0; i < n; i++) x[i] = i + 1;           
+            y[0] = 5;
+            y[1] = 10;
+            y[2] = 8;
+            y[3] = 5;
+            y[4] = 3;
+            y[5] = 8;
+            y[6] = 7;
+            y[7] = 6;
+            y[8] = 9;
+            y[9] = 5;
+            for(i = 0; i < n; i++) chart1.Series[1].Points.AddXY(x[i], y[i]);
+        }    
+
+        private void StartButton_Click(object sender, EventArgs e) { for (double i = x[0]; i < n; i += 0.05) chart1.Series[0].Points.AddXY(i, Poly(i)); }
+
+        private double Poly(double ex)
+        {
+            double sum = 0, mult;
+            int i, j;
+            for (i = 0; i < n; i++)
+            {
+                mult = 1;
+                for (j = 0; j < n; j++) if (j != i) mult *= (ex - x[j]) / (x[i] - x[j]);                
+                sum += y[i] * mult;
+            }
+            return sum;
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
